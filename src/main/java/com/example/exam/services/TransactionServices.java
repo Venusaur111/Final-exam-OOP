c7e447823781f699bb69a3937327a2a526efece0
A.H RAKOTOARINJATO <hei.herisoa@gmail.com>
2026-08-20 16:29:16 +0300

    fix: refactor endpoints, lombok injection and add global exception handler (#4)

--- src/main/java/com/example/exam/services/TransactionServices.java
+++ src/main/java/com/example/exam/services/TransactionServices.java
@@ -3,18 +3,18 @@ package com.example.exam.services;
 import com.example.exam.models.Transaction;
 import com.example.exam.models.TransactionType;
 import com.example.exam.repositories.TransactionRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
+@NoArgsConstructor
 public class TransactionServices {
 
-    private final TransactionRepository transactionRepository;
-
-    public TransactionServices(TransactionRepository transactionRepository) {
-        this.transactionRepository = transactionRepository;
-    }
+    private TransactionRepository transactionRepository;
 
     public List<Transaction> getTransactionsByType(TransactionType type) {
         return transactionRepository.findByTransactionType(type);
